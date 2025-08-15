24ebe15e07e96f9537c37d023675c86ef685af24
sendlE1337 <XamerJKL@gmail.com>
2025-08-15 15:58:07 +0300

    [HANDLE]

---
+++ src/handler/Messages/chained_handler.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file chained_handler.cpp
+ * @brief Реализация методов ChainedHandler
+ */
+
+#include "../include/handler/Messages/chain/chained_handler.h"
+
+void ChainedHandler::add(std::unique_ptr<IMessageHandler> handler)
+{
+  handlers_.emplace_back(std::move(handler));
+}
+
+bool ChainedHandler::handle(std::shared_ptr<Socket> sender, const std::string &msg)
+{
+  for (auto &handler : handlers_)
+  {
+    if (handler->handle(sender, msg))
+    {
+      return true;
+    }
+  }
+  return false;
+}
